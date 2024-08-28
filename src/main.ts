b34b405f2639676aecb5d55ba7184abbdcdce081
ernestovv85 <ernestovv85@gmail.com>
2024-08-28 11:55:33 -0600

    Add new token

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
 
 async function bootstrap() {
+  const logger = new Logger('ProductsMain')
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  logger.log(`Running on port ${envs.port}`);
+
+  await app.listen(3001);
 }
 bootstrap();
